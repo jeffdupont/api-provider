fe9fc911e6d3f7e10c0b1a2d48189e63f31ed341
Jeff Dupont <jeff.dupont@gmail.com>
2015-10-21 13:58:54 -0700

    echo some strings to test with

--- src/Models/ProviderModel.php
+++ src/Models/ProviderModel.php
@@ -3,5 +3,10 @@ namespace Api\Models;
 
 class ProviderModel extends Api\Models\BaseModel
 {
+    public function __construct()
+    {
+        echo __METHOD__ . "\n";
 
+        parent::__construct();
+    }
 }
